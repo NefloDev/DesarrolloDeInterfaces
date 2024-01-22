56909d809046dcbd59a5b5e6ba1284bcf73c82ae
Alejandro Nebot <alneflo27@gmail.com>
2024-01-22 17:31:01 +0100

    Practica2 added TabLayout and ViewPager2

--- Tema5/Practica2/app/src/main/java/com/example/practica2/MainActivity.kt
+++ Tema5/Practica2/app/src/main/java/com/example/practica2/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.practica2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.practica2.databinding.ActivityMainBinding
+import com.example.practica2.fragments.TabFragment1
+import com.example.practica2.fragments.TabFragment2
+import com.example.practica2.fragments.TabFragment3
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager.adapter = object : FragmentStateAdapter(this@MainActivity){
+            override fun getItemCount(): Int {
+                return 3
+            }
+
+            override fun createFragment(position: Int): Fragment {
+                return when(position){
+                    0 -> TabFragment1()
+                    1 -> TabFragment2()
+                    2 -> TabFragment3()
+                    else -> TabFragment1()
+                }
+            }
+        }
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager){tab, position ->
+            tab.text = when(position){
+                0 -> "Tab 1"
+                1 -> "Tab 2"
+                2 -> "Tab 3"
+                else -> "Tab 1"
+            }
+        }.attach()
+
     }
 }
